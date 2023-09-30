89a963cc9f06c7aeab9e3ccb10bc5c4316f0d5fc
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-09-30 13:23:49 +0200

    starting cache

--- app/main.py
+++ app/main.py
@@ -3,12 +3,18 @@ from config import (
     get_settings,
     Settings,
 )  # can use app.config if depending on the file structure and where it is
+from functools import lru_cache
 
 app = FastAPI()
 
 
 @app.get("/ping")
-def pong(settings: Settings = Depends(get_settings)):
+def pong(
+    settings: Settings = Depends(get_settings),
+):  # the pong function depends on Depends, which in turn depends on the get_settings functions being run. The settings variable is then set to the returned get_settings value][=[p42 ]]
+    print(settings.environment)
+    print(settings.testing)
+
     return {
         "ping": "pong!",
         "environment": settings.environment,
